348d8a6f8f2ba69618478763017646e884e1febd
Alberto_Sanchez <alberto.sanchez.p@hotmail.com>
2026-06-24 23:44:14 -0300

    Refactor: Move global arbol CSS stylesheets inside arbol/components/ directory to clean up loose route files

--- app/arbol/layout.js
+++ app/arbol/layout.js
@@ -1,6 +1,6 @@
-import './App.css';
-import './PersonModal.css';
-import './ProfileDrawer.css';
+import './components/App.css';
+import './components/PersonModal.css';
+import './components/ProfileDrawer.css';
 
 export const metadata = {
   title: 'Árbol Genealógico | Galicia Migrante',
